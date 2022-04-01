3a62a09940743849234af8768314f2e427bd0256
JensJenka <jensjk@hotmail.com>
2022-04-01 15:45:23 +0200

    made basic design

---
+++ app/src/main/java/com/example/androideksamenkohortsyv/Globals.kt
@@ -0,0 +1,48 @@
+package com.example.androideksamenkohortsyv
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.core.content.ContextCompat
+import java.io.ByteArrayOutputStream
+import java.io.Serializable
+
+object Globals {
+    val TAG = "AndroidLifeCycle"
+}
+
+data class Picture(var imageUri: String?, var x: Int, var y: Int, var w: Int, var h: Int, var imageH: Int, var imageW: Int, var position: Int=-1):
+    Serializable {
+}
+
+fun VectorDrawableToBitmap(context: Context, id: Int?, uri: String?) : Bitmap {
+    val drawable = (ContextCompat.getDrawable(context!!, id!!) as VectorDrawable)
+    val image = Bitmap.createBitmap(
+        drawable.getIntrinsicWidth(),
+        drawable.getIntrinsicHeight(),
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(image)
+    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+    drawable.draw(canvas)
+
+    return image
+}
+
+fun UriToBitmap(context: Context, id: Int?, uri: String?): Bitmap {
+    val image: Bitmap = MediaStore.Images.Media.getBitmap(context!!.contentResolver, Uri.parse(uri))
+    return image
+}
+
+fun getBitmap(context: Context, id: Int?, uri: String?, decoder: (Context, Int?, String?) -> Bitmap): Bitmap {
+    return decoder(context, id, uri)
+}
+
+fun bitmapTobyteArray(image: Bitmap): ByteArrayOutputStream {
+    val outputStream = ByteArrayOutputStream()
+    image.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+    return outputStream
+}
--- app/src/main/java/com/example/androideksamenkohortsyv/MainActivity.kt
+++ app/src/main/java/com/example/androideksamenkohortsyv/MainActivity.kt
@@ -1,11 +1,89 @@
 package com.example.androideksamenkohortsyv
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    private lateinit var fragmentManager: FragmentManager
+
+    private var pictureArray = ArrayList<Picture>()
+
+    fun switchFragment(v: View) {
+        Log.i(Globals.TAG, "Activity 1 switchFragment. Tag: " + v.getTag().toString())
+        Toast.makeText(
+            this,
+            "Activity switchFragment. Tag" + v.getTag().toString(),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        fragmentManager = supportFragmentManager
+
+  /*      if (Integer.parseInt(v.getTag().toString()) == 1) {
+            fragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragment_main,
+                    Fragment1(),
+                    "Fragment1"
+                )
+                .commit()
+        } else {
+            fragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragment_main,
+                    Fragment2(studentsInfo),
+                    "Fragment2"
+                )
+                .commit()
+        }*/
+    }
+
+    /*fun upload(view: View) {
+        var imageUri =
+            (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).imageUri.toString()
+
+        var rect = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).actualCropRect!!
+        var imgW = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).image.width
+        var imgH = (fragmentManager.findFragmentByTag("Fragment1") as Fragment1).image.height
+
+        val newPicture: Picture = Picture(
+            imageUri,
+            rect.left.toInt(),
+            rect.top.toInt(),
+            rect.right.toInt(),
+            rect.bottom.toInt(),
+            imgW.toInt(),
+            imgH.toInt()
+        )
+        pictureArray.add(newPicture)
+
+       /* dbHelper?.writableDatabase?.insert("students", null, ContentValues().apply {
+            put("name", newStudent.name)
+            put("surname", newStudent.surname)
+            put(
+                "image",
+                bitmapTobyteArray(
+                    getBitmap(
+                        applicationContext,
+                        null,
+                        newStudent.imageUri,
+                        ::UriToBitmap
+                    )
+                ).toByteArray()
+            )
+        })
+
+        Toast.makeText(this, "Added New Student", Toast.LENGTH_SHORT).show() */
+    }*/
+
 }
