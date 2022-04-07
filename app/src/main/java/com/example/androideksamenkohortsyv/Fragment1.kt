206a32d6a2563b5b428581ef837bb507decfc927
JensJenka <jensjk@hotmail.com>
2022-04-07 13:18:09 +0200

    uploading picture works, will not save state

--- app/src/main/java/com/example/androideksamenkohortsyv/Fragment1.kt
+++ app/src/main/java/com/example/androideksamenkohortsyv/Fragment1.kt
@@ -1,18 +1,31 @@
 package com.example.androideksamenkohortsyv
 
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+
+
+
 
 class Fragment1 : Fragment() {
 
+    public lateinit var imageView: ImageView
+    public lateinit var button: Button
+    public var imageUri: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         Log.i(Globals.TAG, "Fragment 1 onCreate")
         Toast.makeText(activity, "Fragment 1 onCreate", Toast.LENGTH_SHORT).show()
@@ -25,8 +38,34 @@ class Fragment1 : Fragment() {
         Log.i(Globals.TAG, "Fragment 1 onCreateView")
         Toast.makeText(activity, "Fragment 1 onCreateView", Toast.LENGTH_SHORT).show()
 
-        var image = view?.findViewById<ImageView>(R.id.image)
-        return inflater.inflate(R.layout.fragment1, container, false)
+        var view = inflater.inflate(R.layout.fragment1, container, false)
+
+        imageView = view.findViewById<ImageView>(R.id.imageView)
+        imageView.setOnClickListener(View.OnClickListener {
+            var i = Intent()
+            i.type = "*/*"
+            i.action = Intent.ACTION_GET_CONTENT
+
+            startForResult.launch(i)
+        })
+
+
+        return view
     }
 
+    var startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        imageUri = it.data?.data.toString()
+
+        var bitmap_image = getBitmap(requireContext(), null, imageUri, ::UriToBitmap)
+
+        imageView.layoutParams = imageView.layoutParams.apply {
+
+            width = bitmap_image.width
+            height = bitmap_image.height
+        }
+
+        imageView.setImageBitmap(bitmap_image)
+        imageView.background = BitmapDrawable(resources, bitmap_image)
+    }
 }
--- app/src/main/java/com/example/androideksamenkohortsyv/UploadActivity.kt
+++ app/src/main/java/com/example/androideksamenkohortsyv/UploadActivity.kt
@@ -3,14 +3,34 @@ package com.example.androideksamenkohortsyv
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.edmodo.cropper.CropImageView
+import android.content.Intent
+import android.widget.Button
+
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+
+
 
 class UploadActivity : AppCompatActivity(){
+
+    private var imageView: ImageView? = null
+    private val SELECT_PICTURE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment1)
+        imageView = findViewById(R.id.image)
+/*
+        var image = view?.findViewById<ImageView>(android.R.id.image)
+        var selectButton = findViewById(R.id.select_btn)
+        findViewById(R.id.select_btn).setOnClickListener(View.OnClickListener { openImageChooser()
+
+        })
 
         val uploadPicture : Picture = (intent.getSerializableExtra("TEST picture") as Picture)
 
@@ -30,9 +50,16 @@ class UploadActivity : AppCompatActivity(){
                 uploadPicture.h
             )
 
-        }
-        imageView.setImageBitmap(image)
+        }*/
+       // imageView.setImageBitmap(image)
 
+    }
+
+    fun openImageChooser() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE)
     }
 
 
